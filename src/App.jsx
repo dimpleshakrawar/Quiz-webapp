f8ee31d0c4cd97a3d2b3e7d6bf882933535f306f
dimpleshakrawar <dimpleshakrawar@gmail.com>
2023-09-01 18:34:16 +0530

    project done

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
-import { RouterProvider } from "react-router-dom";
-import router from "./router";
+import MainLayout from "./MainLayout";
 
 function App() {
-  return <RouterProvider router={router} fallbackElement={<p>Loading...</p>} />;
+  return (
+    <>
+      <MainLayout />
+    </>
+  );
 }
 
 export default App;
